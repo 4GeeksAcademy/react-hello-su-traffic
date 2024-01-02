92fdc795970ea07cc3b3e8aa8332b840eb66bf98
Suri1336 <146222294+Suri1336@users.noreply.github.com>
2024-01-02 01:02:38 +0000

    done with no button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [activeLight, setActiveLight]= useState("Red")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="main ">
+<div className="Stick"></div>
+			<div className=" Whole text-center">
+			<div className={activeLight==="Red"? "Red Light Glow":"Red Light"} onClick={()=>setActiveLight("Red")}></div>
+			<div className={activeLight==="Yellow"? "Yellow Light Glow":"Yellow Light"} onClick={()=>setActiveLight("Yellow")}></div>
+			<div className={activeLight==="Green"? "Green Light Glow":"Green Light"} onClick={()=>setActiveLight("Green")}></div>
+		</div>
 		</div>
 	);
 };
