230524bc415d2e9377eee9c1d5d67f83c78f7d2f
Suri1336 <146222294+Suri1336@users.noreply.github.com>
2024-01-05 22:29:33 +0000

    completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React,{useState} from "react";
+// import Button from "./button";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,6 +7,20 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	const [activeLight, setActiveLight]= useState("Red")
+	function handleClick(e){
+		e.preventDefault()
+		if(activeLight=="Red"){
+			setActiveLight("Yellow")
+			
+		}
+		else if (activeLight=="Yellow"){
+			setActiveLight("Green")
+		}
+		else {
+			setActiveLight("Red")
+		}
+
+	}
 	return (
 		<div className="main ">
 <div className="Stick"></div>
@@ -14,6 +29,8 @@ const Home = () => {
 			<div className={activeLight==="Yellow"? "Yellow Light Glow":"Yellow Light"} onClick={()=>setActiveLight("Yellow")}></div>
 			<div className={activeLight==="Green"? "Green Light Glow":"Green Light"} onClick={()=>setActiveLight("Green")}></div>
 		</div>
+		{/* <Button /> */}
+<button onClick={(e)=> handleClick(e)}>Traffic Light</button>
 		</div>
 	);
 };
